c8ed3ddbce8821421bb3d9e401050aeaaae047df
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-07 23:34:13 +0200

    gradient

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const movieDb = axios.create({
-  baseUrl: 'https://api.themoviedb.org/3/movie',
-  params: {
-    api_key: '36301517ac72412e514152b6beb0a473',
-    language: 'es-ES',
-  },
+
+const movieDB = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/movie',
+    params: {
+        api_key: '1865f43a0549ca50d341dd9ab8b29f49',
+        language: 'es-ES'
+    }
 });
 
-export default movieDb;
+
+export default movieDB;
+
+
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import { HomeScreen } from '../screens/HomeScreen';
 import { DetailScreen } from '../screens/DetailScreen';
+import { Movie } from '../interfaces/movieInterface';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+  HomeScreen: undefined;
+  DetailScreen: Movie;
+}
 
-export const Navigation=() =>{
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigation = () => {
   return (
     <Stack.Navigator
-    screenOptions={{
+      screenOptions={{
         headerShown: false,
-        cardStyle:{
-            backgroundColor: 'white'
-        }
-    }}
+      }}
     >
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="DetailScreen" component={DetailScreen} />
+      <Stack.Screen name="HomeScreen" component={ HomeScreen } />
+      <Stack.Screen name="DetailScreen" component={ DetailScreen } />
     </Stack.Navigator>
   );
 }
